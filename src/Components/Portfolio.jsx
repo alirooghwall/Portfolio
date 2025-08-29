28a80133860e0c0da6bfc770e728560a30fd75d2
Ali Ahmad <aliroghwal@gmail.com>
2025-08-29 05:21:54 +0000

    Added username and repository

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/laptop-day.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Laptop on table in daylight";
 
 /**
  * Project list
@@ -31,12 +31,12 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My first Project: Library Management System",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
+      "A Library Management System built to manage books, members, and borrowing records, as my first programming project.",
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
-  {
+  /*{
     title: "Web Development for Beginners",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
@@ -48,12 +48,12 @@ const projectList = [
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
-  {
+  ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,{
     title: "GitHub Codespaces and github.dev",
     description:
       "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
     url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+  */},
 ];
 
 const Portfolio = () => {
