b946fd07ce3cfee98f2ba129c4526bfca2660264
Ali Ahmad <aliroghwal@gmail.com>
2025-08-29 06:22:00 +0000

    fix text

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -56,29 +56,28 @@ const projectList = [
   },*/
 ];
 
-const Portfolio = () => {
-  return (
-    <section className="padding" id="portfolio">
-      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
-      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
-        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-          <img 
-          src={image} 
-          style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out 0s 1 slideInLeft" }} />
-        </div>
-        <div className="container">
-          {projectList.map((project) => (
-            <div className="box" key={project.title}>
-              <a href={project.url} target="_blank" rel="noopener noreferrer">
-                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
-              </a>
-              <p className="small">{project.description}</p>
-            </div>
-          ))}
-        </div>
+const Portfolio = () => (
+  <section className="min-height padding" style={{ background: "#fff", color: "#222" }} id="portfolio">
+    <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+    <p style={{ textAlign: "center" }}>Here are some of my projects and work samples.</p>
+    <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+      <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+        <img 
+        src={image} 
+        style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out 0s 1 slideInLeft" }} alt={imageAltText}/>
       </div>
-    </section>
-  );
-};
+      <div className="container">
+        {projectList.map((project) => (
+          <div className="box" key={project.title}>
+            <a href={project.url} target="_blank" rel="noopener noreferrer">
+              <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+            </a>
+            <p className="small">{project.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>
+);
 
 export default Portfolio;
